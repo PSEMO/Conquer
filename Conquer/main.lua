484f0485c004c9772152b813b3aac3acd2b5f31a
Semih <semihyilmaz032@gmail.com>
2022-11-22 15:58:46 +0300

    Smallest enemy radius is 3

--- Conquer/main.lua
+++ Conquer/main.lua
@@ -5,17 +5,17 @@
 -----------------------------------------------------------------------------------------
 
 
---Physics.
+--physics
 local physics = require( "physics" )
 physics.start()
 physics.setGravity( 0, 0 )
 
---Background.
+--background
 local background = display.newImageRect("pixel2.png", 1.5 * display.contentWidth, 1.5 * display.contentHeight)
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
---Player.
+--player
 local player = display.newRect(0, 0,  4, 4)
 local playerRadius = 4
 local PlayerEaten = 2
@@ -24,18 +24,16 @@ player:setFillColor( 1, 1, 0 )
 player.x = display.contentCenterX
 player.y = display.contentCenterY
 
---Players gun.
 local gun = display.newRect(0, 0, playerRadius / 3, playerRadius)
 gun:setFillColor(0, 0, 0, 0)
 gun.anchorY = playerRadius
 
---Auto attack timer UI display.
 local autoAttackUI = display.newRect(0, 0, playerRadius * 2, playerRadius / 4)
 local autoAttackStopWatch = 0
 local autoAttackCoolDown = 1 * 1000
 autoAttackUI:setFillColor(0, 0, 0, 0)
 
---Only player and bullets are dynamic, to be able to detect those collisions
+--Only player is dynamic, to be able to detect collision
 physics.addBody( player, "dynamic", {radius=2, isSensor = true })
 
 --Current point player collected (is corralated to player object radius)
@@ -44,16 +42,14 @@ local maxPointCount = 0
 local pointText = display.newText( pointCount, display.contentCenterX, 20, native.systemFont, 40 )
 pointText:setFillColor( 0.2, 0.4, 0.8 )
 
---Rush timer UI display.
-local rushUI = display.newRect(0, 0, playerRadius * 2, playerRadius / 4)
-
 --Default speed and current speed
+local rushUI = display.newRect(0, 0, playerRadius * 2, playerRadius / 4)
 local moveSpeed = 1
 local defaultSpeed = 1
-
---Rush duration
 local SpeedDuration = 1
 local canRush = true
+
+local EndText = display.newText( "", display.contentCenterX, display.contentCenterY - 90, native.systemFont, 100 )
 
 --holds all keys. True if they are pressed down or being held down. false if not
 local keyDownList = {}
@@ -68,7 +64,6 @@ local pointObjNumber = 0
 --Holds the next enemy id (also holds the last id of enemy_List array)
 local enemyID = 1
 
---if we just shoot, timer goes off at playerUpdate
 local shot = false
 
 --vision limiter, using a png for it
@@ -76,33 +71,35 @@ local vision = display.newImageRect("vision.png", 2732, 2732)
 vision.x = display.contentCenterX
 vision.y = display.contentCenterY
 
---Delta.time and last frames event.time to calculate delta.time
 local deltaTime  = 0
 local lastTime = 0
 
---Upgrade visuals
 local AttackSpeedUI = display.newRoundedRect(0, 0, 160, 12, 2)
 local AttackPenetrationUI = display.newRoundedRect(0, 0, 160, 12, 2)
 local RushSpeedUI = display.newRoundedRect(0, 0, 160, 12, 2)
---Upgrade visuals
+
 local AttackSpeedText = display.newText("[1] - higher attack speed", display.contentCenterX, 25, native.systemFont, 12)
 local AttackPenetrationText = display.newText("[2] - higher bullet penetration", display.contentCenterX, 25, native.systemFont, 12)
 local RushSpeedText = display.newText("[3] - higher rush speed", display.contentCenterX, 25, native.systemFont, 12)
+--local RushDurationText = display.newText("[4] - higher rush speed", display.contentCenterX, 25, native.systemFont, 12)
 
---Penetration number that player can deal
 local penetrationUpgrade = 1
 
---Positions LevelUp UI elements in playerUpdate(), if its true
 local LevelUpUI = false
 
 local speedMultiplier = 2.4
 
---Does nothing for now
 local isGameEnded = false
 
---Called when a key is pressed
+local DeadScreenForeGround = display.newImageRect("pixel.png", display.contentWidth * 2, display.contentHeight * 2)
+DeadScreenForeGround.x = display.contentCenterX
+DeadScreenForeGround.y = display.contentCenterY
+
+DeadScreenForeGround:setFillColor( 0, 0, 0, 0)
+
+--called when a key is pressed
 function keyHandler(event)
-    --Saves a key and if it is pressed down or not (true if button is down, false if button is not down)
+    --saves if a key is pressed down or not
     if event.phase == "up" then
         keyDownList[event.keyName] = false
         print(event.keyName)
@@ -112,10 +109,8 @@ function keyHandler(event)
     end
 end
 
---Creates upgrade visuals
 function CreateLevelSelectionUI()
 
-    --We delete old visuals (we can just not create them if they already exist too, I just didn't have enough debug time)
     DeleteLevelSelectionUI()
     LevelUpUI = true
     
@@ -123,8 +118,8 @@ function CreateLevelSelectionUI()
     if(playerRadius / 4 > 5) then
         scaleForUI = 5
     end
+    --playerRadius * 2
 
-    --AttackSpeed upgrade visuals settings
     AttackSpeedText = display.newText( "[1] - higher attack speed", display.contentCenterX, 20, native.systemFont, 12 )
     AttackSpeedText:setFillColor(1, 1, 1)
     AttackSpeedUI = display.newRoundedRect(0, 0, 170, 15, 2)
@@ -132,7 +127,6 @@ function CreateLevelSelectionUI()
     AttackSpeedUI.strokeWidth = 3
     AttackSpeedUI:setStrokeColor(0, 0, 0, 0.7)
     
-    --AttackPenetration upgrade visuals settings
     AttackPenetrationText = display.newText( "[2] - higher bullet penetration", display.contentCenterX, 20, native.systemFont, 12 )
     AttackPenetrationText:setFillColor(1, 1, 1)
     AttackPenetrationUI = display.newRoundedRect(0, 0, 170, 15, 2)
@@ -140,7 +134,6 @@ function CreateLevelSelectionUI()
     AttackPenetrationUI.strokeWidth = 3
     AttackPenetrationUI:setStrokeColor(0, 0, 0, 0.7)
     
-    --RushSpeed upgrade visuals settings
     RushSpeedText = display.newText( "[3] - higher rush speed", display.contentCenterX, 20, native.systemFont, 12 )
     RushSpeedText:setFillColor(1, 1, 1)
     RushSpeedUI = display.newRoundedRect(0, 0, 170, 15, 2)
@@ -149,7 +142,6 @@ function CreateLevelSelectionUI()
     RushSpeedUI:setStrokeColor(0, 0, 0, 0.7)
 end
 
---Deletes upgrade visuals
 function DeleteLevelSelectionUI()
     
     LevelUpUI = false
@@ -164,18 +156,13 @@ function DeleteLevelSelectionUI()
     DeleteGivenObj(RushSpeedText)
 end
 
---Runs every frame
 function playerUpdate(event)
-
-    --deleting other frames time from the new one to get delta time (current frames time)
     deltaTime = event.time - lastTime
     lastTime = event.time
 
 --Button presses
 
-    --makes current displacement speed "speedMultiplier" times the default if given button is pressed and
-    --if there is duration left
-    --if duration goes below 500 * 0.1 player needs to wait until its back 100%
+    --makes current displacement speed "speedMultiplier" times the default if given buttons are pressed
     if keyDownList["leftShift"] and canRush then
         moveSpeed = defaultSpeed * speedMultiplier
         SpeedDuration = SpeedDuration - deltaTime
@@ -197,7 +184,7 @@ function playerUpdate(event)
     end
     rushUI.xScale = SpeedDuration / 500
 
-    --shoots if hasAGun() and we did not just shoot
+
     if keyDownList["space"] then
         if hasAGun() then
             if not shot then
@@ -221,17 +208,8 @@ function playerUpdate(event)
     if keyDownList["down"] or keyDownList["s"] then
         player.y = player.y + moveSpeed 
     end
-
-    --if(isGameEnded) then
-    --    keyDownList["r"]
-    --end
-
-    --if keyDownList["l"] then
-    --    pointChanged(10)
-    --end
 --Others
 
-    --sets a timer and changes autoAttackUI.xScale according to it
     if(shot)
     then
         autoAttackStopWatch = autoAttackStopWatch + deltaTime
@@ -246,13 +224,11 @@ function playerUpdate(event)
         end
     end
 
-    --rush and shoot UI distance in between
     local distance = playerRadius * 0.5
     if(playerRadius / 4 > 5) then
         distance = 22 * 0.5
     end
 
-    --if player has leveled up checks level up buttons
     if(LevelUpUI)
     then
         local isChosenUpgrade = false
@@ -274,16 +250,13 @@ function playerUpdate(event)
         --    isChosenUpgrade = true
         --end
 
-        --to make them stay after selection
         if(isChosenUpgrade) then
             DeleteLevelSelectionUI()
         end
     end
 
---location things
-
-    --locates UIs, gun and vision according to player pos
-
+-- location things
+    
     EnemyTowardsPlayer()
 
     vision.x = player.x
@@ -320,7 +293,6 @@ function playerUpdate(event)
     end
 end
 
---Bullet reaches to a destination within bullettime
 function Shoot()
 
     local bulletTime = 10000
@@ -334,12 +306,15 @@ function Shoot()
 	newLaser:toFront()
     ReOrderToFront()
 
-    --destination is decided by rotations direction * 2000
 	transition.to( newLaser, {
     x = player.x + math.cos(math.rad(playerRotation + 270)) * 2000,
     y = player.y + math.sin(math.rad(playerRotation + 270)) * 2000,
     time=bulletTime
 	} )
+
+    --print( "rotation: "..(playerRotation + 270))
+    --print( "x: "..math.cos(math.rad(playerRotation + 270)) )
+    --print( "y: "..math.sin(math.rad(playerRotation + 270)) )
 
     newLaser.type = "bullet"
     newLaser.r = playerRadius / 5
@@ -354,8 +329,6 @@ function DeleteGivenObj(ObjToDelete)
     display.remove(ObjToDelete)
 end
 
---Create object at given place.
---Only used for creating two objects; one green infront of player, one red at the back of the player.
 function createObjectThere(type, aX, aY)
     local r = 3
     if(type == "point") then
@@ -383,6 +356,8 @@ function createObjectThere(type, aX, aY)
         EnemyList[enemyID] = CurrentObject
 
         enemyID = enemyID + 1
+
+        r = math.random(3, 7)
     end
 
     CurrentObject.r = r;
@@ -390,12 +365,15 @@ function createObjectThere(type, aX, aY)
     timer.performWithDelay(1, function() addRB(CurrentObject, r) end)
 end
 
---Creates a random object in random position and size within screen limits
+--crates in random position and sizes within screen limits
 function createObject(type)
     
     local x = math.random(0, display.contentWidth)
     local y = math.random(0, display.contentHeight)
-    local r = math.random(1 , 5 + math.log(playerRadius))--add a code to not spawn these near player.x player.y----------------------------------------
+    local r = math.random(1 , 5 + math.log(playerRadius))--add a code to not spawn these near player.x player.y----------------------------------------    
+    
+    if(type == "enemy") then r = math.random(3, 7) end
+
     local CurrentObject = display.newCircle(x, y, r)
     
     --if a point is to be created
@@ -427,7 +405,7 @@ function createObject(type)
     timer.performWithDelay(1, function() addRB(CurrentObject, r) end)
 end
 
---We add static collider to detect collision. Only used to addBody with delay
+--We add static collider to detect collision
 function addRB(ObjToChange, radius)
 
     physics.addBody( ObjToChange, "static", {radius=radius, isSensor = true })
@@ -493,12 +471,11 @@ function createPlayer()
     ReOrderToFront()
 end
 
---True if player has a gun, false if not
 function hasAGun()
     return (playerRadius > 8)
 end
 
---(event.object1 and event.object2 collides in this engine)
+--(event.object1 and event.object2 collided in this engine)
 local function onGlobalCollision( event )---------------------------------------------------------------------------------------
 
     if ( event.phase == "began" )
@@ -549,8 +526,7 @@ local function onGlobalCollision( event )---------------------------------------
                 end
             end
 
-        else--A bullet is collided with something
-            
+        else
 
             local bullet
 
@@ -592,7 +568,6 @@ local function onGlobalCollision( event )---------------------------------------
     end
 end
 
---Get bigger
 function levelUp()
 
     while PlayerEaten > playerRadius
@@ -638,7 +613,7 @@ function CheckPointAndEnemy()
     ReOrderToFront()
 end
 
---makes these appear before others in the given order
+--makes these appear before others
 function ReOrderToFront()
 
     vision:toFront()
@@ -664,7 +639,7 @@ function ReOrderToFront()
     end
 end
 
---travels all enemies and places them front since enemyID stores next id to be used, enemyID is also array.length
+--travels all enemies and places them front since enemyID stores next id to be used, its also array.length
 function EnemyToFront()
     local i = 1
     while i < enemyID
@@ -677,8 +652,6 @@ function EnemyToFront()
     end
 end
 
---travels all enemies and makes them move towards player.
---Speed is according to their radius
 function EnemyTowardsPlayer()
     local i = 1
 
@@ -702,7 +675,6 @@ function EnemyTowardsPlayer()
     end
 end 
 
---Shows score
 local function KYS()
 
     local EndText = display.newText( "", display.contentCenterX, display.contentCenterY, native.systemFont, 100 )
@@ -710,7 +682,6 @@ local function KYS()
     gameEnded(EndText)
 end
 
---Shows "you won!""
 local function YouWon()
 
     local EndText = display.newText( "", display.contentCenterX, display.contentCenterY, native.systemFont, 100 )
@@ -718,8 +689,6 @@ local function YouWon()
     gameEnded(EndText)
 end
 
---Removes event listeners creates a transparent black image over everything
---Deletes vision and point shower text
 function gameEnded(EndText)
 
     --mouse listener does not work on editor but is replacemant for touch listener in final build
@@ -731,10 +700,6 @@ function gameEnded(EndText)
 
     isGameEnded = true
 
-    local DeadScreenForeGround = display.newImageRect("pixel.png", display.contentWidth * 2, display.contentHeight * 2)
-    DeadScreenForeGround.x = display.contentCenterX
-    DeadScreenForeGround.y = display.contentCenterY
-
     DeadScreenForeGround:setFillColor( 0, 0, 0, 0.5 )
     DeadScreenForeGround:toFront()
 
@@ -744,6 +709,7 @@ function gameEnded(EndText)
 
     EndText:setFillColor(0.392, 0.392, 0.863)--Bright midnight blue
     EndText:toFront()
+
     --player dies here.
 end
 
@@ -764,7 +730,6 @@ function pointChanged(x)
         end
     end
 
-    --opens upgrade screen if score is mod of 4, for the first time for every number.
     if(maxPointCount > 10 and maxPointCount == pointCount)
     then
         if(math.fmod(maxPointCount, 4) == 0)
@@ -789,7 +754,6 @@ function AngleBetweenTwoPoints(aX, aY, bX, bY)
     return ((math.atan2(aY - bY, aX - bX) * 57.29578) - 90)
 end
 
---This also is magic, just don't touch it
 function RadianBetweenTwoPoints(aX, aY, bX, bY)
 
     return (math.atan2(aY - bY, aX - bX))
@@ -812,4 +776,4 @@ Runtime:addEventListener( "mouse", RotateToMousePos)--DON'T FORGET TO CHANGE REM
 --Runtime:addEventListener( "touch", RotateToMousePos)--works only when left click is held down
 Runtime:addEventListener( "collision", onGlobalCollision )
 Runtime:addEventListener("key", keyHandler)
-Runtime:addEventListener("enterFrame", playerUpdate)    
+Runtime:addEventListener("enterFrame", playerUpdate)
